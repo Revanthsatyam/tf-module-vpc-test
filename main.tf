9b1a9e3382fa1525f4e047bed697a8d2bc611690
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-24 13:23:11 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -17,11 +17,17 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "main" {
   for_each = var.subnets
-  vpc_id = aws_vpc.main.id
+  vpc_id   = aws_vpc.main.id
 
   tags = {
     Name = each.key
   }
+}
+
+resource "aws_route_table_association" "main" {
+  for_each       = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null), "id", null)
 }
